7e3b44c91f4c15820280094e54aaa3e1208151f4
Maximilian Weber <mw2852@columbia.edu>
2017-02-20 16:14:28 -0500

    uncomment deleter

--- routes/deleter.js
+++ routes/deleter.js
@@ -9,13 +9,13 @@ var finalImages = path.join(__dirname, '..', 'public', 'images');
 var removeSessionFiles = function(sessionId){
     if(sessionId && sessionId.length > 20){
         console.log(deleteFiles + ' ' + sessions + '/' + sessionId + ' ' + finalImages + '/' + sessionId);
-        // var c = child.exec(deleteFiles + ' ' + sessions + '/' + sessionId + ' ' + finalImages + '/' + sessionId);
-        // c.on('exit', function(code){
-        //     if(code !== 0){
-        //         console.log('Failed to delete files for ' + sessionId);
-        //         //___ERROR___
-        //     }
-        // });
+        var c = child.exec(deleteFiles + ' ' + sessions + '/' + sessionId + ' ' + finalImages + '/' + sessionId);
+        c.on('exit', function(code){
+            if(code !== 0){
+                console.log('Failed to delete files for ' + sessionId);
+                //___ERROR___
+            }
+        });
     }
 };
 
